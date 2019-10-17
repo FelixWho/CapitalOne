3abaf1a9df2f7d019226daca600f10e0962fc1f4
FelixWho <twodragonhu@gmail.com>
2019-10-17 01:14:54 -0500

    bug fixes

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost', 'wustl_inst', 'wustl_pass', 'capitalone');
+$mysqli = new mysqli('localhost', 'summit', 'capitalone', 'capitalone');
 if($mysqli->connect_errno) {
 	printf("Connection Failed: %s\n", $mysqli->connect_error);
 	exit;
